8782b29d563d79cf1dd018a75c7749b291a5f62e
playstation451 <98996547+playstation452@users.noreply.github.com>
2024-02-07 20:45:14 -0500

    updated version

--- src/main/java/com/client/wdnc/ExampleMod.java
+++ src/main/java/com/client/wdnc/ExampleMod.java
@@ -23,7 +23,7 @@ public class ExampleMod
 {
     public static final String MODID = "wdnc";
     public static final String NAME = "WDNC Hack Client";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.1";
 
     public static ExampleMod instance;
     public SettingsManager settingsManager;
